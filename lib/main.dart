8e026f2cc0b127851eab4a3df64e34b036bc34e2
Wamitinewton <wamitinewton@gmail.com>
2024-08-05 19:21:39 +0300

    secure storage dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,300 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+enum _Actions { deleteAll, isProtectedDataAvailable }
+
+enum _ItemActions { delete, edit, containsKey, read }
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  final _storage = const FlutterSecureStorage();
+  final _accountNameController =
+      TextEditingController(text: 'flutter_secure_storage_service');
+
+  List<_SecItem> _items = [];
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  void initState() {
+    super.initState();
+    _accountNameController.addListener(() => _readAll());
+    _readAll();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  Future<void> _readAll() async {
+    final all = await _storage.readAll(
+      iOptions: _getIOSOptions(),
+      aOptions: _getAndroidOptions(),
+    );
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _items = all.entries
+          .map((entry) => _SecItem(entry.key, entry.value))
+          .toList(growable: false);
     });
   }
 
+  Future<void> _deleteAll() async {
+    await _storage.deleteAll(
+        iOptions: _getIOSOptions(), aOptions: _getAndroidOptions());
+    _readAll();
+  }
+
+  Future<void> _isProtectedDataAvailable() async {
+    final scaffold = ScaffoldMessenger.of(context);
+    final result = await _storage.isCupertinoProtectedDataAvailable();
+    scaffold.showSnackBar(SnackBar(
+      content: Text('Protected data available: $result'),
+      backgroundColor: result != null && result ? Colors.green : Colors.red,
+    ));
+  }
+
+  Future<void> _addNewItem() async {
+    final String key = _randomValue();
+    final String value = _randomValue();
+
+    await _storage.write(
+        key: key,
+        value: value,
+        iOptions: _getIOSOptions(),
+        aOptions: _getAndroidOptions());
+    _readAll();
+  }
+
+  IOSOptions _getIOSOptions() => IOSOptions(
+        accountName: _getAccountName(),
+      );
+  AndroidOptions _getAndroidOptions() => const AndroidOptions(
+        encryptedSharedPreferences: true,
+      );
+
+  String? _getAccountName() =>
+      _accountNameController.text.isEmpty ? null : _accountNameController.text;
+
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Secure sec tutorial'),
+          actions: <Widget>[
+            IconButton(
+                key: const Key('add_random'),
+                onPressed: _addNewItem,
+                icon: const Icon(Icons.add)),
+            PopupMenuButton<_Actions>(
+              key: const Key('popup_menu'),
+              onSelected: (action) {
+                switch (action) {
+                  case _Actions.deleteAll:
+                    _deleteAll();
+                    break;
+                  case _Actions.isProtectedDataAvailable:
+                    _isProtectedDataAvailable();
+                    break;
+                }
+              },
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<_Actions>>[
+                const PopupMenuItem(
+                  key: Key('delete_all'),
+                  value: _Actions.deleteAll,
+                  child: Text('Delete all'),
+                ),
+                const PopupMenuItem(
+                  key: Key('is_protected_data_available'),
+                  value: _Actions.isProtectedDataAvailable,
+                  child: Text('IsProtectedDataAvailable'),
+                ),
+              ],
             ),
           ],
         ),
+        body: Column(
+          children: [
+            if (!kIsWeb && Platform.isIOS)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextFormField(
+                  controller: _accountNameController,
+                  decoration:
+                      const InputDecoration(labelText: 'kSecAttrService'),
+                ),
+              ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _items.length,
+                itemBuilder: (BuildContext context, int index) => ListTile(
+                  trailing: PopupMenuButton(
+                    key: Key('popup_row_$index'),
+                    onSelected: (_ItemActions action) =>
+                        _perfomAction(action, _items[index], context),
+                    itemBuilder: (BuildContext context) =>
+                        <PopupMenuEntry<_ItemActions>>[
+                      PopupMenuItem(
+                        value: _ItemActions.delete,
+                        child: Text(
+                          'Delete',
+                          key: Key('delete_row_$index'),
+                        ),
+                      ),
+                      PopupMenuItem(
+                        value: _ItemActions.edit,
+                        child: Text(
+                          'Edit',
+                          key: Key('edit_row_$index'),
+                        ),
+                      ),
+                      PopupMenuItem(
+                        value: _ItemActions.containsKey,
+                        child: Text(
+                          'Contains Key',
+                          key: Key('contains_row_$index'),
+                        ),
+                      ),
+                      PopupMenuItem(
+                        value: _ItemActions.read,
+                        child: Text(
+                          'Read',
+                          key: Key('contains_row_$index'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  title: Text(
+                    _items[index].value,
+                    key: Key('title_row_$index'),
+                  ),
+                  subtitle: Text(
+                    _items[index].key,
+                    key: Key('subtitle_row_$index'),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+  Future<void> _perfomAction(
+      _ItemActions action, _SecItem item, BuildContext context) async {
+    switch (action) {
+      case _ItemActions.delete:
+        await _storage.delete(
+            key: item.key,
+            iOptions: _getIOSOptions(),
+            aOptions: _getAndroidOptions());
+        _readAll();
+
+        break;
+      case _ItemActions.edit:
+        if (!context.mounted) return;
+        final result = await showDialog<String>(
+          context: context,
+          builder: (context) => _EditItemsWidget(item.value),
+        );
+        if (result != null) {
+          await _storage.write(
+              key: item.key,
+              value: result,
+              iOptions: _getIOSOptions(),
+              aOptions: _getAndroidOptions());
+          _readAll();
+        }
+        break;
+      case _ItemActions.containsKey:
+        final key = await _displayTextInputDialog(context, item.key);
+        final result = await _storage.containsKey(key: key);
+        if (!context.mounted) return;
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Contains Key: $result, key checked: $key'),
+          backgroundColor: result ? Colors.green : Colors.red,
+        ));
+        break;
+      case _ItemActions.read:
+        final key = await _displayTextInputDialog(context, item.key);
+        final result =
+            await _storage.read(key: key, aOptions: _getAndroidOptions());
+        if (!context.mounted) return;
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('value: $result')),
+        );
+        break;
+    }
+  }
+
+  Future<String> _displayTextInputDialog(
+      BuildContext context, String key) async {
+    final controller = TextEditingController();
+    controller.text = key;
+    await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Check if key exists'),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('ok'))
+            ],
+            content: TextField(
+              controller: controller,
+            ),
+          );
+        });
+    return controller.text;
+  }
+
+  String _randomValue() {
+    final rand = Random();
+    final codeUnits = List.generate(20, (index) {
+      return rand.nextInt(26) + 65;
+    });
+    return String.fromCharCodes(codeUnits);
+  }
+}
+
+class _EditItemsWidget extends StatelessWidget {
+  _EditItemsWidget(String text)
+      : _controller = TextEditingController(text: text);
+
+  final TextEditingController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Edit Item'),
+      content: TextField(
+        key: const Key('title field'),
+        controller: _controller,
+        autofocus: true,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      actions: <Widget>[
+        TextButton(
+          key: const Key('cancel'),
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+            key: const Key('save'),
+            onPressed: () => Navigator.of(context).pop(_controller.text),
+            child: const Text('Save'))
+      ],
     );
   }
+}
+
+class _SecItem {
+  _SecItem(this.key, this.value);
+  final String key;
+  final String value;
 }
